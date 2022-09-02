9cc9326f064ccdf94ddd1d9427b65f4c7dacf893
Stuckonyou98 <angelcoldplay5@gmail.com>
2022-09-02 21:54:01 +0630

    Managing User Profile

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Backend\UserController;
+use App\Http\Controllers\Backend\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,13 +37,20 @@ Route::get('/admin/logout', [AdminController::class, 'Logout'])->name('admin.log
 Route::prefix('users')->group(function () {
 
     Route::get('/view', [UserController::class, 'UserView'])->name('user.view');
-
     Route::get('/add', [UserController::class, 'UserAdd'])->name('users.add');
-
     Route::post('/store', [UserController::class, 'UserStore'])->name('users.store');
-
     Route::get('/edit/{id}', [UserController::class, 'UserEdit'])->name('users.edit');
     Route::post('/update/{id}', [UserController::class, 'UserUpdate'])->name('users.update');
-
     Route::get('/delete/{id}', [UserController::class, 'UserDelete'])->name('users.delete');
+});
+
+/// User Profile and Change Password
+
+Route::prefix('profile')->group(function () {
+
+    Route::get('/view', [ProfileController::class, 'ProfileView'])->name('profile.view');
+    Route::get('/edit', [ProfileController::class, 'ProfileEdit'])->name('profile.edit');
+    Route::post('/store', [ProfileController::class, 'ProfileStore'])->name('profile.store');
+    Route::get('/password/view', [ProfileController::class, 'PasswordView'])->name('password.view');
+    Route::post('/password/update', [ProfileController::class, 'PasswordUpdate'])->name('password.update');
 });
